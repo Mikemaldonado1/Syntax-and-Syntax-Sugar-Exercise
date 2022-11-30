b2e11dbba2a69c806be428fea758ad894af07805
Michael Maldonado <MichaelFrankMaldonado@gmail.com>
2022-11-30 13:38:56 -0700

    exercises

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,6 +5,60 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            /*          
+
+                       inferred typing
+                       string interpolation
+                       and the ternary operator
+
+
+                       int answer = 4;
+                       string response;
+
+                       if (answer < 9) 
+                       {
+                           response = answer + " is less than nine";
+                       }
+
+                       else
+                       {
+                           response = answer + " is greater than or equal to nine";
+                       }
+
+
+           */
+
+            int answer = 4;
+
+
+            //Inferred Typing
+
+
+            var response = "";
+            Console.WriteLine(response);
+
+            //String Interpolation
+
+            int answer2 = 4;
+
+            var response2 = (answer2 < 9)? $"{answer2} is less than nine" : $"{answer2} is greater than or equal to nine";  
+            Console.WriteLine(response2);
+
+
+
+            //Tertiary Operator
+
+            int answer3 = 4;
+
+            var response3 = (answer3< 9) ? "Your answer is less than nine" : "Your answer is greater than or equal to nine";  
+            Console.WriteLine(response3);
+                
+
+
+
+
         }
     }
 }
